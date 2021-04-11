5ea948d5b5ecc16ad7d7b17ea0a94cb4fdcdb124
MathiasK <mathiasmundtkofod@gmail.com>
2021-04-11 22:38:20 +0200

    Here we go again
    -Imported the Modern UI Pack from unity asset store
    -Created a resources folder, which so far contains one card prefab and two suit psd files
    -Created a CardDisplay class, which handles how cards are displayed
    -Created a GameController class, which in the future will handle game func
    -Created a MyResources class, that acts like a singleton, and handles shared resources that needs to be available public. These resources include things such as the sprites for different suits.
    -Created a card template in photoshop.

---
+++ Beer Academy/Assets/Scripts/CardDisplay.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardDisplay : MonoBehaviour
+{
+
+        [Header("Card Info")]
+        public Suit suit;
+
+        [Header("Card Elements")] [Tooltip("")]
+        public List<Image> suits;
+
+
+
+        private void Start()
+        {
+                FindSuitElements(transform);
+        }
+
+        private void Update()
+        {
+                if (Input.GetKeyDown(KeyCode.Space))
+                {
+                        SetSuit(Suit.Diamond);
+                }
+        }
+
+        private void SetSuit(Suit s)
+        {
+                foreach (Image image in suits)
+                {
+                        image.sprite = MyResources.Current.GetSprite(s);
+                }
+        }
+
+
+        private void FindSuitElements(Transform t)
+        {
+                if (t.CompareTag("Suit"))
+                {
+                        suits.Add(t.gameObject.GetComponent<Image>());
+                }
+
+                if (t.childCount > 0)
+                {
+                        foreach (Transform child in t)
+                        {
+                                FindSuitElements(child);
+                        }
+                }
+        }
+}
+
+
+
+public enum Suit
+{
+        Club,
+        Heart,
+        Spade,
+        Diamond
+}
---
+++ Beer Academy/Assets/Scripts/MyResources.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+public class MyResources : MonoBehaviour
+{
+    public static MyResources Current;
+    private void Awake()
+    {
+        if (Current == null)
+        {
+            Current = this;
+        }
+        
+        InitializeResources();
+    }
+
+    private readonly Dictionary<Suit, Sprite> _suits = new Dictionary<Suit, Sprite>();
+    
+    private void InitializeResources()
+    {
+        var sprites = Resources.LoadAll("Suits", typeof(Sprite));
+
+        foreach (Object sprite in sprites)
+        {
+            Debug.Log(sprite.name);
+
+            string enumString = sprite.name.Split('_')[1];
+            
+            Suit parsedEnum = (Suit) Enum.Parse( typeof(Suit), enumString );
+            
+            _suits.Add(parsedEnum,(Sprite) sprite);
+        }
+    }
+
+    public Sprite GetSprite(Suit s)
+    {
+        return _suits[s];
+    }
+}
