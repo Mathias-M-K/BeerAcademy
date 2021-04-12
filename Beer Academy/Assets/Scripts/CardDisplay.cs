908b10af1b8521f13f849f9c5d8119be18b0dcfd
MathiasK <mathiasmundtkofod@gmail.com>
2021-04-12 16:19:06 +0200

    Added suits and improved the MyResources class
    -Added a little more structure to the project
    
    -MyResources class is not able to detect what color the suit sprites and in turn, it now offers a GetSuitColor method, which returns the color of a given suit, so that the color of the rank will be updated to the correct color
    
    -Card display have been extended to also store the rank elements from the card, so that their color can be edited later.
    
    -Added a txt file with the address for the Adobe XD prototype.

--- Beer Academy/Assets/Scripts/CardDisplay.cs
+++ Beer Academy/Assets/Scripts/CardDisplay.cs
@@ -1,63 +1,71 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.Serialization;
 using UnityEngine.UI;
 
 public class CardDisplay : MonoBehaviour
 {
+    private Suit _suit;
 
-        [Header("Card Info")]
-        public Suit suit;
+    private int _currentSuit = 0;
+    
+    [HideInInspector] public List<Image> suits;
+    [HideInInspector] public List<TextMeshProUGUI> ranks;
 
-        [Header("Card Elements")] [Tooltip("")]
-        public List<Image> suits;
+    private void Start()
+    {
+        FindCardElements(transform);
+    }
 
-
-
-        private void Start()
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-                FindSuitElements(transform);
+            Suit s = (Suit) _currentSuit;
+            SetSuit(s);
+
+            _currentSuit++;
+
+            _currentSuit = _currentSuit == Enum.GetValues(typeof(Suit)).Length ? 0 : _currentSuit;
+        }
+    }
+
+    private void SetSuit(Suit s)
+    {
+        foreach (Image image in suits)
+        {
+            image.sprite = MyResources.Current.GetSprite(s);
+        }
+        
+        foreach (TextMeshProUGUI rank in ranks)
+        {
+            rank.color = MyResources.Current.GetSuitColor(s);
+        }
+        
+    }
+    
+    private void FindCardElements(Transform t)
+    {
+        if (t.CompareTag("Suit"))
+        {
+            suits.Add(t.gameObject.GetComponent<Image>());
         }
 
-        private void Update()
+        if (t.CompareTag("Rank"))
         {
-                if (Input.GetKeyDown(KeyCode.Space))
-                {
-                        SetSuit(Suit.Diamond);
-                }
+            ranks.Add(t.gameObject.GetComponent<TextMeshProUGUI>());
         }
 
-        private void SetSuit(Suit s)
+        if (t.childCount > 0)
         {
-                foreach (Image image in suits)
-                {
-                        image.sprite = MyResources.Current.GetSprite(s);
-                }
+            foreach (Transform child in t)
+            {
+                FindCardElements(child);
+            }
         }
-
-
-        private void FindSuitElements(Transform t)
-        {
-                if (t.CompareTag("Suit"))
-                {
-                        suits.Add(t.gameObject.GetComponent<Image>());
-                }
-
-                if (t.childCount > 0)
-                {
-                        foreach (Transform child in t)
-                        {
-                                FindSuitElements(child);
-                        }
-                }
-        }
+    }
 }
 
 
-
-public enum Suit
-{
-        Club,
-        Heart,
-        Spade,
-        Diamond
-}
--- Beer Academy/Assets/Scripts/MyResources.cs
+++ Beer Academy/Assets/Scripts/MyResources.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using Object = UnityEngine.Object;
 
@@ -18,25 +19,77 @@ public class MyResources : MonoBehaviour
     }
 
     private readonly Dictionary<Suit, Sprite> _suits = new Dictionary<Suit, Sprite>();
+    private Dictionary<Suit, Color> _suitColors = new Dictionary<Suit, Color>();
     
+    /// <summary>
+    /// Method for fething resources from the resources folder and making them accessible globally.
+    /// </summary>
     private void InitializeResources()
     {
         var sprites = Resources.LoadAll("Suits", typeof(Sprite));
 
+        
+        SpriteRenderer sr = GetComponent<SpriteRenderer>();
         foreach (Object sprite in sprites)
         {
-            Debug.Log(sprite.name);
-
             string enumString = sprite.name.Split('_')[1];
             
+            //Parsing the string as an enum of type Suit
             Suit parsedEnum = (Suit) Enum.Parse( typeof(Suit), enumString );
             
+            //Adding the sprite with it's enum counterpart in a key,pair set, so that it is accessible globally
             _suits.Add(parsedEnum,(Sprite) sprite);
+
+            //Using to attached sprite renderer it to extract pixel-data from sprite
+            sr.sprite = (Sprite) sprite;
+            Color[] data = sr.sprite.texture.GetPixels();
+            
+            //Getting the suit color and adding it to the dictionary
+            Color suitColor = FindSpriteColor(data);
+            _suitColors.Add(parsedEnum,suitColor);
         }
+        
+        //Removing the sprite renderer, cuz we don't need that
+        Destroy(sr);
+    }
+
+    /// <summary>
+    /// Checks every 1000 pixel, and returns the first color it finds where the albedo value is one (meaning that it is not transparent)
+    /// </summary>
+    /// <param name="pixelData"></param>
+    /// <returns>The Primary Color of the Sprite</returns>
+    /// <exception cref="Exception"></exception>
+    private Color FindSpriteColor(Color[] pixelData)
+    {
+        for (int i = 0; i < pixelData.Length; i += 1000)
+        {
+            Color c = pixelData[i];
+
+            if (c.a == 1)
+            {
+                return c;
+            }
+        }
+
+        throw new Exception("Couldn't find color");
     }
 
     public Sprite GetSprite(Suit s)
     {
         return _suits[s];
     }
+
+    public Color GetSuitColor(Suit s)
+    {
+        return _suitColors[s];
+    }
+}
+
+public enum Suit
+{
+    Club,
+    Heart,
+    Spade,
+    Diamond,
+    Penis
 }
